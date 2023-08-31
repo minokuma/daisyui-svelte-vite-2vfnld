c766efaada216c10c7143eee933a49b52fc7448f
minokuma <ksky216@naver.com>
2023-08-31 15:13:19 +0900

    1 file modified

--- src/reveal/Example1.svelte
+++ src/reveal/Example1.svelte
@@ -129,7 +129,7 @@
                           <div class="navbar flex-none w-full pt-6 {$isDarkMode ? "bg-base-200": "bg-neutral-100" } text-neutral-content">
                               <div class="flex-none">
                                   <label for="mobile-drawer" class="btn btn-circle btn-ghost drawer-button">
-                                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
+                                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current {$isDarkMode ? "text-neutral-100": "text-base-200" }">
                                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
                                       </svg>
                                   </label>
@@ -139,7 +139,7 @@
                               </div>
                               <div class="flex-none">
                                   <button class="btn btn-circle btn-ghost">
-                                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
+                                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current {$isDarkMode ? "text-neutral-100": "text-base-200" }">
                                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                                       </svg>
                                   </button>
@@ -169,18 +169,18 @@
                             <a class="flex-grow tab tab-lifted"></a>
                           </div>
                           <div class="grid grid-cols-3 gap-2 p-2 pt-4 {$isDarkMode ? "bg-base-200": "bg-neutral-100" }">
-                            <img src="https://picsum.photos/id/1004/200/200">
-                            <img src="https://picsum.photos/id/1003/200/200">
-                            <img src="https://picsum.photos/id/1002/200/200">
-                            <img src="https://picsum.photos/id/1001/200/200">
-                            <img src="https://picsum.photos/id/1000/200/200">
-                            <img src="https://picsum.photos/id/999/200/200">
-                            <img src="https://picsum.photos/id/998/200/200">
-                            <img src="https://picsum.photos/id/997/200/200">
-                            <img src="https://picsum.photos/id/996/200/200">
-                            <img src="https://picsum.photos/id/995/200/200">
-                            <img src="https://picsum.photos/id/994/200/200">
-                            <img src="https://picsum.photos/id/993/200/200">
+                            <img class="rounded-md" src="https://picsum.photos/id/1004/200/200">
+                            <img class="rounded-md" src="https://picsum.photos/id/1003/200/200">
+                            <img class="rounded-md" src="https://picsum.photos/id/1002/200/200">
+                            <img class="rounded-md" src="https://picsum.photos/id/1001/200/200">
+                            <img class="rounded-md" src="https://picsum.photos/id/1000/200/200">
+                            <img class="rounded-md" src="https://picsum.photos/id/999/200/200">
+                            <img class="rounded-md" src="https://picsum.photos/id/998/200/200">
+                            <img class="rounded-md" src="https://picsum.photos/id/997/200/200">
+                            <img class="rounded-md" src="https://picsum.photos/id/996/200/200">
+                            <img class="rounded-md" src="https://picsum.photos/id/995/200/200">
+                            <img class="rounded-md" src="https://picsum.photos/id/994/200/200">
+                            <img class="rounded-md" src="https://picsum.photos/id/993/200/200">
                           </div>
                         </div>
                           <ul class="menu justify-center flex-none w-full px-3 horizontal ">
