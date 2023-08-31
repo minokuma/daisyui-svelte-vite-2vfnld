3400c556b6f7f830bec67e42a05c41db1b841250
minokuma <ksky216@naver.com>
2023-08-31 18:43:53 +0900

    2 files modified

--- src/App.svelte
+++ src/App.svelte
@@ -681,20 +681,14 @@
       {/if}
     </div>
     <!-- 푸터 시작 -->
-    <footer class="relative px-60 py-10  footer {$isDarkMode ? "bg-zinc-700": "bg-zinc-100" }">
-      <!-- <div class="grid grid-flow-col gap-4">
-        <a class="link link-hover">소개</a> 
-        <a class="link link-hover">문의</a> 
-        <a class="link link-hover">채용</a> 
-        <a class="link link-hover">언론보도</a>
-      </div>  -->
-      <div>
-        <div>
+    <footer class="relative px-6 py-10 footer {$isDarkMode ? "bg-zinc-700": "bg-zinc-100" }">
+      <div class="md:flex md:justify-between">
+        <div class="mb-6 md:mb-0 md:ml-70">
           <p class="{$isDarkMode ? "text-zinc-200": "text-zinc-800" } text-2xl mb-4">마인스페이스(주)</p>
           <p class="{$isDarkMode ? "text-zinc-400": "text-zinc-500" } mb-2">대표자 : 엄정현 / 사업자 등록번호 : 643-87-02688 / 광주광역시 동구 동계천로 150 i-PLEX 306</p>
           <p class="{$isDarkMode ? "text-zinc-400": "text-zinc-500" }">통신판매신고번호 제 2023-광주동구-0110호</p>
         </div>
-        <div class="grid grid-flow-col gap-4">
+        <div class="md:flex md:gap-4 grid grid-flow-col gap-4">
           <a>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current {$isDarkMode ? "text-zinc-400": "text-zinc-500" }">
               <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
@@ -712,7 +706,6 @@
           </a>
         </div>
       </div> 
-      
     </footer>
     <!-- 푸터 종료 -->
   </div>
